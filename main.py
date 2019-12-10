a729e7ca334c8ae4ee2b3b0fa60e8900d989ccc5
ianlcz <yann.lecoz@ynov.com>
2019-12-10 17:34:04 +0100

    UPDATE 10/12/2019
    Calcul de l'adresse du réseau et de broadcast, du nombre d'adresses utilisables ainsi que la plage d'adresse.

--- main.py
+++ main.py
@@ -15,40 +15,51 @@ def convert_binary_to_decimal(string):
 ip = "192.168.0.1"
 binary_ip = convert_decimal_to_binary(ip)
 
-# Rewriting of the Subnet mask
-abr_subnetMask = 24
-list_origin_binary_subnetMask = []
-for element in range(abr_subnetMask):
-    list_origin_binary_subnetMask.append('1')
-for element in range(32 - abr_subnetMask):
-    list_origin_binary_subnetMask.append('0')
-list_binary_subnetMask = []
+# Rewriting of the Netmask
+abr_Netmask = 24
+list_origin_binary_Netmask = []
+for element in range(abr_Netmask):
+    list_origin_binary_Netmask.append('1')
+for element in range(32 - abr_Netmask):
+    list_origin_binary_Netmask.append('0')
+list_binary_Netmask = []
 counter = 0
-for index in range(len(list_origin_binary_subnetMask)):
-    list_binary_subnetMask.append(list_origin_binary_subnetMask[index])
+for index in range(len(list_origin_binary_Netmask)):
+    list_binary_Netmask.append(list_origin_binary_Netmask[index])
     counter += 1
     if counter == 8 and index != 31:
-        list_binary_subnetMask.append('.')
+        list_binary_Netmask.append('.')
         counter = 0
-binary_subnetMask=''.join(list_binary_subnetMask)
-subnetMask=convert_binary_to_decimal(binary_subnetMask)
+binary_Netmask=''.join(list_binary_Netmask)
+Netmask=convert_binary_to_decimal(binary_Netmask)
 
 # Calculation of the Network address
 list_networkAddress = []
 for index in range(len(ip.split('.'))):
-    list_networkAddress.append(str(binary_to_decimal(decimal_to_binary(int(ip.split('.')[index]) & int(subnetMask.split('.')[index]), 8))))
+    list_networkAddress.append(str(binary_to_decimal(decimal_to_binary(int(ip.split('.')[index]) & int(Netmask.split('.')[index]), 8))))
 networkAddress = '.'.join(list_networkAddress)
 binary_networkAddress = convert_decimal_to_binary(networkAddress)
 
 # Calculation of the Broadcast address
 binary_broadcastAddress = []
-for index in range(len(binary_subnetMask)):
-    if binary_subnetMask[index] == '1' or binary_subnetMask[index] == '.':
+for index in range(len(binary_Netmask)):
+    if binary_Netmask[index] == '1' or binary_Netmask[index] == '.':
         binary_broadcastAddress.append(binary_ip[index])
     else:
         binary_broadcastAddress.append('1')
 binary_broadcastAddress = ''.join(binary_broadcastAddress)
 broadcastAddress = convert_binary_to_decimal(binary_broadcastAddress)
 
-#print(f"DÉCIMAL\t\tBINAIRE\n{ip}\t{binary_ip}\n{subnetMask}\t{binary_subnetMask}\n{networkAddress}\t{binary_networkAddress}\n{broadcastAddress}\t{binary_broadcastAddress}")
-print(f"Adresse IP\t\t{ip}\nMasque de sous-réseaux\t{subnetMask}\nAdresse du réseau\t{networkAddress}\nAdresse de broadcast\t{broadcastAddress}")
+# Definition of the number of usable addresses
+binary_usableAddresses = []
+for element in binary_Netmask:
+    if element == '0':
+        binary_usableAddresses.append(element)
+numberOfUsableAddresses = int(convert_binary_to_decimal(''.join(binary_usableAddresses).replace('0', '1'))) - 1
+
+# Definition of the address range
+minimalAddress = networkAddress[:-1] + str(int(networkAddress.split('.')[-1]) + 1)
+maximalAddress = broadcastAddress[:-3] + str(int(broadcastAddress.split('.')[-1]) - 1)
+addressRange = minimalAddress + " - " + maximalAddress
+
+print(f"Adresse IP\t\t\t{ip}\nMasque de sous-réseaux\t\t{Netmask}\nAdresse du réseau\t\t{networkAddress}\nAdresse de broadcast\t\t{broadcastAddress}\nNombre d'adresses utilisables\t{numberOfUsableAddresses}\nPlage d'adresses\t\t{addressRange}")
